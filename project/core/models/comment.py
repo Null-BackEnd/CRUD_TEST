999465834efa7291a65d66725f7ebfa643c0be3f
태라 <terra2007@naver.com>
2022-10-23 01:03:35 +0900

    📑 :: comment entity 수정

--- project/core/models/comment.py
+++ project/core/models/comment.py
@@ -1,14 +1,14 @@
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from CRUD_TEST.project.core.models import Base
 
 class Comment(Base):
     __tablename__ = "tbl_comment"
 
-    detail = Column(String(300), nullable=False)
+    content = Column(String(300), nullable=False)
 
-    user_id = Column(ForeignKey("user.id"), primary_key=True, index=True)
-    content_id = Column(ForeignKey("content.id"), primary_key=True)
+    user_id = Column(ForeignKey("tbl_user.id"), primary_key=True, index=True)
+    feed_id = Column(ForeignKey("tbl_feed.id"), primary_key=True)
 
-    user = relationship("User")
-    content = relationship("Content")
+    user = relationship("User", backref=backref("writed_comments"), foreign_keys=user_id)
+    feed = relationship("Feed", backref=backref("feed_comments"), foreign_keys=feed_id)
