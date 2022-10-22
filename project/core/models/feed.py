632e0072cfa4343b013cecf4fb192ced86fc18d1
태라 <terra2007@naver.com>
2022-10-23 01:03:16 +0900

    📑 :: content -> feed entity 수정

---
+++ project/core/models/feed.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, INT, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from CRUD_TEST.project.core.models import Base
+
+class Feed(Base):
+    __tablename__ = "tbl_feed"
+
+    id = Column(INT, autoincrement=True, primary_key=True)
+    content = Column(String(3000), nullable=False)
+
+    user_id = Column(ForeignKey("tbl_user.id"), primary_key=True)
+
+    user = relationship("User", backref=backref("writed_feeds"), foreign_keys=user_id)
