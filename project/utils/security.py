60689c5bf1dbf7c13eb86e8c4e10a452aacce5a8
태라 <terra2007@naver.com>
2022-10-25 21:49:23 +0900

    📑 :: utils security 수정

--- project/utils/security.py
+++ project/utils/security.py
@@ -44,14 +44,3 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         if not user.scalar():
             raise credentials_exception
         return user.first()
-
-def check_feed(feed_id: int, user_id: int, session: Session):
-    feed = session.query(Feed).filter(Feed.id==feed_id).first()
-
-    if not feed:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="FEED NOT FOUNDED")
-
-    if feed.user_id == user_id:
-        return feed
-
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="THE USER IS NOT VALID")
