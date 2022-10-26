2dcf941483c72f30b9eec46f99cadb59544b5100
kiwi._.jw <kiwi200512@gmail.com>
2022-10-26 23:37:56 +0900

    📑 :: comment app 수정

--- project/app/comment.py
+++ project/app/comment.py
@@ -1,9 +1,10 @@
 from project.core.models import session_scope
 from project.core.schemas import comment
 from project.core.models.user import User
-from project.utils.comment import mk_comment
-from project.utils.comment import mod_comment
-from project.utils.comment import del_comment
+from project.core.models.feed import Feed
+from project.utils.comment import make_comment
+from project.utils.comment import modify_comment
+from project.utils.comment import delete_comment
 
 from fastapi import APIRouter
 
@@ -11,23 +12,22 @@ router = APIRouter()
 
 
 @router.post("")
-async def mk_comment(body: comment, user=User):
+async def make_comment(body: comment, user=User, feed=Feed):
     with session_scope() as session:
-        request = mk_comment(comment=body.comment, user_id=user.id, session=session)
+        request = make_comment(comment=body.comment, post_id=feed.id, user_id=user.id, session=session)
 
         return request
 
 @router.put("")
-async def mod_comment(body: comment, user=User):
+async def modify_comment(body: comment, user=User, feed=Feed):
     with session_scope() as session:
-        request = mod_comment(comment=body.comment, user_id=user.id, session=session)
+        request = modify_comment(comment=body.comment, post_id=feed.id, user_id=user.id, session=session)
 
         return request
 
 @router.delete("")
-async def del_comm(body: comment, user=User):
+async def delete_comment(body: comment, feed=Feed, user=User):
     with session_scope() as session:
-        requests = del_comment(comment=body.comment, user_id=user.id, session=session)
+        requests = delete_comment(comment=body.comment, feed=feed.id, user_id=user.id, session=session)
 
         return requests
-
