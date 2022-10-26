62fe7ea0d4fe2bf9a7e5759bb7bbddcf5e216395
kiwi._.jw <kiwi200512@gmail.com>
2022-10-26 23:38:23 +0900

    📑 :: comment utils 수정

--- project/utils/comment.py
+++ project/utils/comment.py
@@ -1,9 +1,12 @@
 from project.core.models.comment import Comment
-
+from project.core.models.user import User
 from sqlalchemy.orm import Session
 
 
-def mk_comment(user_id: int, content: str, session: Session):
+def is_my_comment(user_id=int, user=User):
+    return True if user_id == user.id else False
+
+def make_comment(user_id: int, content: str, session: Session):
     new_comment = Comment(
         content=content,
         user_id=user_id
@@ -16,12 +19,13 @@ def mk_comment(user_id: int, content: str, session: Session):
         "message": "success"
     }
 
-def del_comment(user_id: int, content: str, session: Session):
-    del_commt = session.query(content)
-    session.delete(del_commt)
+def delete_comment(user_id: int, content: str, session: Session):
+    is_my_comment(user_id)
+    delete_comment = session.query(content)
+    session.delete(delete_comment)
     session.commit()
 
-def mod_comment(user_id: int, content: str, session: Session):
-    mod_commt = session.query(content)
-
-    mod_commt.update_one({content: content})
+def modify_comment(user_id: int, content: str, session: Session):
+    is_my_comment(user_id)
+    modify_comment = session.query(content).filter()
+    modify_comment.update_one({content: content})
