d3f96da2056e927942636e66e874d3a05c03bd4a
Jeong._.wxx <kiwi200512@gmail.com>
2022-10-28 22:46:46 +0900

    📑 :: comment utils 수정

--- project/core/schemas/comment.py
+++ project/core/schemas/comment.py
@@ -10,6 +10,6 @@ class WriteComment(BaseModel):
 class ModifyComment(BaseModel):
     id: int
     comment_id: int
-    comment_id: int
+    post_id: int
     user_id: int
     content: constr(min_length=1, max_length=300)
