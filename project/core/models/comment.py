d9264381b730fdf679de46c86a7c9fbc9ebf5f9d
Jeong._.wxx <kiwi200512@gmail.com>
2022-10-29 00:46:15 +0900

    📑 :: comment models 수정

--- project/core/models/comment.py
+++ project/core/models/comment.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, INT
 from sqlalchemy.orm import relationship, backref
-from CRUD_TEST.project.core.models import Base
+from project.core.models import Base
 
 class Comment(Base):
     __tablename__ = "tbl_comment"
 
+    id = Column(INT, autoincrement=True, primary_key=True)
     content = Column(String(300), nullable=False)
-
-    user_id = Column(ForeignKey("tbl_user.id"), primary_key=True, index=True)
-    feed_id = Column(ForeignKey("tbl_feed.id"), primary_key=True)
+    user_id = Column(ForeignKey("tbl_user.id"))
+    feed_id = Column(ForeignKey("tbl_feed.id"))
 
     user = relationship("User", backref=backref("writed_comments"), foreign_keys=user_id)
     feed = relationship("Feed", backref=backref("feed_comments"), foreign_keys=feed_id)
