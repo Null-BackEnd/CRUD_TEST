685aeb730e36791097635c4c220c528fa5c7c7b3
Jeong._.wxx <kiwi200512@gmail.com>
2022-10-28 15:19:35 +0900

    📑 :: comment schemas 수정

--- project/core/schemas/comment.py
+++ project/core/schemas/comment.py
@@ -2,8 +2,14 @@ from pydantic import BaseModel, constr
 
 class WriteComment(BaseModel):
     id: int
+    comment_id: int
+    post_id: int
+    user_id: int
     content: constr(min_length=1, max_length=300)
 
 class ModifyComment(BaseModel):
     id: int
+    comment_id: int
+    comment_id: int
+    user_id: int
     content: constr(min_length=1, max_length=300)
